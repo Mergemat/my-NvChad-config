7834f633dd13787a221cab0a371319a836e52011
Омаров Багаутдин <ng.ghh@yandex.ru>
2024-06-04 12:53:43 +0300

    add page labels to tabufline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -20,6 +20,25 @@ M.ui = {
       { name = "codeium" },
     },
   },
+  tabufline = {
+    order = { "treeOffset", "buffers", "nextjsLabel" },
+    modules = {
+      nextjsLabel = function()
+        local current_buf = vim.api.nvim_get_current_buf()
+        local bufname = vim.api.nvim_buf_get_name(current_buf)
+        if string.match(bufname, "/app/") then
+          local dirname = string.match(bufname, "/app/(.*)/page.tsx")
+          if dirname == nil then
+            return "index - Page"
+          end
+          dirname = string.gsub(dirname, "%[locale%]/", "")
+
+          return string.format("%s - Page", dirname)
+        end
+        return ""
+      end,
+    },
+  },
 
   -- hl_override = {
   -- 	Comment = { italic = true },
