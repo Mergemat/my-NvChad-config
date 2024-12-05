fcd0f8b01e0ec22629713520a5ec06abac8c3754
Омаров Багаутдин <ng.ghh@yandex.ru>
2024-12-05 16:05:39 +0300

    rm cloak

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,12 +2,6 @@ return {
   { "wakatime/vim-wakatime", lazy = false },
   --
   {
-    "laytan/cloak.nvim",
-    event = "VeryLazy",
-    opts = {},
-  },
-  --
-  {
     "max397574/better-escape.nvim",
     event = "InsertEnter",
     config = function()
