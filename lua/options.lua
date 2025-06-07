cf3f1f89797c71da79f92c1680b52b3131ae17ad
Омаров Багаутдин <ng.ghh@yandex.ru>
2025-06-07 19:35:03 +0300

    remove keymaps for "w"

--- lua/options.lua
+++ lua/options.lua
@@ -15,3 +15,35 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", { fg = "#6CC644" })
+
+local function keymap_exists_for_buffer(mode, lhs, buf)
+  for _, keymap in ipairs(vim.api.nvim_buf_get_keymap(buf, mode)) do
+    if keymap.lhs == lhs then
+      return true
+    end
+  end
+  return false
+end
+
+local function delete_keymaps(args)
+  vim.schedule(function()
+    local buffer = args.buf
+    local keymaps_to_delete = {
+      " wa",
+      " wr",
+      " wl",
+    }
+
+    for _, key_to_del in ipairs(keymaps_to_delete) do
+      if keymap_exists_for_buffer("n", key_to_del, buffer) then
+        vim.keymap.del("n", key_to_del, { buffer = buffer })
+      end
+    end
+  end)
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    delete_keymaps(args)
+  end,
+})
