7f097bc2afdccad32d4d4942715213fca1ff7f24
Омаров Багаутдин <ng.ghh@yandex.ru>
2026-01-06 15:20:25 +0300

    opencode

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -4,29 +4,6 @@ return {
     event = "VeryLazy",
     opts = {
       startVisible = true,
-      -- showBlankVirtLine = true,
-      -- highlightColor = { link = "Comment" },
-      -- hints = {
-      --      Caret = { text = "^", prio = 2 },
-      --      Dollar = { text = "$", prio = 1 },
-      --      MatchingPair = { text = "%", prio = 5 },
-      --      Zero = { text = "0", prio = 1 },
-      --      w = { text = "w", prio = 10 },
-      --      b = { text = "b", prio = 9 },
-      --      e = { text = "e", prio = 8 },
-      --      W = { text = "W", prio = 7 },
-      --      B = { text = "B", prio = 6 },
-      --      E = { text = "E", prio = 5 },
-      -- },
-      -- gutterHints = {
-      --     G = { text = "G", prio = 10 },
-      --     gg = { text = "gg", prio = 9 },
-      --     PrevParagraph = { text = "{", prio = 8 },
-      --     NextParagraph = { text = "}", prio = 8 },
-      -- },
-      -- disabled_fts = {
-      --     "startify",
-      -- },
     },
   },
 
@@ -83,6 +60,54 @@ return {
     lazy = false,
     config = function()
       require("nvim-ts-autotag").setup {}
+    end,
+  },
+
+  {
+    "NickvanDyke/opencode.nvim",
+    dependencies = {
+      -- Recommended for `ask()` and `select()`.
+      -- Required for `snacks` provider.
+      ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+    },
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+      }
+
+      -- Required for `opts.events.reload`.
+      vim.o.autoread = true
+
+      -- Recommended/example keymaps.
+      vim.keymap.set({ "n", "x" }, "<C-a>", function()
+        require("opencode").ask("@this: ", { submit = true })
+      end, { desc = "Ask opencode" })
+      vim.keymap.set({ "n", "x" }, "<C-x>", function()
+        require("opencode").select()
+      end, { desc = "Execute opencode action…" })
+      vim.keymap.set({ "n", "t" }, "<C-.>", function()
+        require("opencode").toggle()
+      end, { desc = "Toggle opencode" })
+
+      vim.keymap.set({ "n", "x" }, "go", function()
+        return require("opencode").operator "@this "
+      end, { expr = true, desc = "Add range to opencode" })
+      vim.keymap.set("n", "goo", function()
+        return require("opencode").operator "@this " .. "_"
+      end, { expr = true, desc = "Add line to opencode" })
+
+      vim.keymap.set("n", "<S-C-u>", function()
+        require("opencode").command "session.half.page.up"
+      end, { desc = "opencode half page up" })
+      vim.keymap.set("n", "<S-C-d>", function()
+        require("opencode").command "session.half.page.down"
+      end, { desc = "opencode half page down" })
+
+      -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o".
+      vim.keymap.set("n", "+", "<C-a>", { desc = "Increment", noremap = true })
+      vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement", noremap = true })
     end,
   },
 }
